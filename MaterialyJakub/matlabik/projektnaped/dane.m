8df6e6111df70e9bba963f8f49636c1bd7d05098
FORTISow <166801364+FORTISow@users.noreply.github.com>
2024-04-12 11:02:56 +0200

    zmiany, zmiany, zmiany

--- MaterialyJakub/matlabik/projektnaped/dane.m
+++ MaterialyJakub/matlabik/projektnaped/dane.m
@@ -1,20 +1,19 @@
-close all, clear all, clc
 %% Dane projektu 
 %% Dane silnika
-Pn=17; % [kW] !!
-nn=700; % [obr/min]
+Pn=26; % [kW] !!
+nn=625; % [obr/min]
 Un=230; % [V]
-In=85; % [A]
-Js=0.75; % [kgm2]
-Rt=0.253; % [Ohm]
-Lt=1.9*10^(-3); % [H]
+In=132; % [A]
+Js=1.25; % [kgm2]
+Rt=0.202; % [Ohm]
+Lt=1.9; % [H]
 %% obliczone parametry 
-w_n=nn*2*pi/60;
-psi_en=(Un-Rt*In)/w_n; % [Wb]
+omega_n=nn*2*pi/60;
+psi_en=(Un-Rt*In)/omega_n; % [Wb]
 Mn=psi_en*In; % [Nm]
 T=Lt/Rt;
 J=22*Js;
-B=J*Rt/psi_en^2;
+B=J*Rt/psi_en^2
 
 % przyjete dane
 lambda=1.8;
@@ -22,12 +21,12 @@ p=50;
 % Ograniczenia
 Imax = lambda*In;
 dIdtmax = p*In;
-omega_max = w_n;
+omega_max = omega_n;
 
 
 % założone dane do regulatora
 Y=10/(2.5*In);
-Kt=10/(1.2*w_n);
+Kt=10/(1.2*omega_n);
 Kp=0.15*Un;
 % _________________
 J=11*Js; %[kgm2]
@@ -72,33 +71,8 @@ xlabel('t [s]');ylabel('I[A]');
 legend('pochodna prądu twornika','Ograniczenie pochodnej prądu twornika')
 grid on
 figure
-plot(t,odpw,t,w_n.*ones(1,length(t)));
+plot(t,odpw,t,omega_n.*ones(1,length(t)));
 title('odpowiedź skokowa prędkości obrotowej');
 xlabel('t [s]');ylabel('\omega [rad/s]');
 legend('prędkość obrotowa','znamionowa prędkość obrotowa')
 grid on
-
-
-
-% Do obczajenia czy sie przyda
-
-%PUNKT 6
-% 
-% %nastawy regulatora prądu
-% 
-% T1=0.5*B*(1-sqrt(1-(4*T/B)));
-% beta=lambdan/p;
-% B1=B-T1;
-% Kz=(B1-beta)/(Y*B1);
-% m=T1;
-% V=((Kp*Y)/R1)*((B*beta)/(B1-beta));
-% uz0=lambdan*In*((Y*B1)/(B1-beta))
-% 
-% %regulator predkosci P:
-% 
-% KwP=Mn/(psie*Kz*KT*6.078)
-% 
-% %regulator predkosci PI:
-% 
-% KwPI=J/(2*beta*psie*KT*Kz)
-% TrPI=4*beta
